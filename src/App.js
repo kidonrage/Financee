f0c53c6343ebf12882f4f57dcff54248b4838987
Vlad Eliseev <freshymanchan@gmail.com>
2020-04-25 15:57:58 +1000

    Custom theme added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react'
 import { Router } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import { ThemeProvider } from '@material-ui/styles'
 import history from './utils/history'
 import routes from './routes'
+import theme from './theme'
 
 function App() {
   return (
     <Router history={history}>
       <CssBaseline />
-      {renderRoutes(routes)}
+      <ThemeProvider theme={theme}>
+        {renderRoutes(routes)}
+      </ThemeProvider>
     </Router>
   )
 }
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { green } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: green[600],
+    },
+    secondary: {
+      main: '#11cb5f',
+    },
+  },
+});
