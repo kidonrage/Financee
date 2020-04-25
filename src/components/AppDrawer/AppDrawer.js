15a6865e3096cb3992e9d3846ba4c8098e92c4d4
Vlad Eliseev <freshymanchan@gmail.com>
2020-04-25 15:50:33 +1000

    Expandable drawer implemented

--- src/components/AppDrawer/AppDrawer.js
+++ src/components/AppDrawer/AppDrawer.js
@@ -1,65 +1,34 @@
 import React from 'react'
 import clsx from 'clsx'
-import { makeStyles, useTheme } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import Drawer from '@material-ui/core/Drawer'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
 import List from '@material-ui/core/List'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
-import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
 import InboxIcon from '@material-ui/icons/MoveToInbox'
 import MailIcon from '@material-ui/icons/Mail'
+import MenuCloseIcon from '@material-ui/icons/MenuOpen';
+import MenuOpenIcon from '@material-ui/icons/Menu';
 import styles from './styles'
+import TopBar from '../TopBar'
 
 const useStyles = makeStyles(styles)
 
 const AppDrawer = ({children}) => {
   const classes = useStyles()
-  const theme = useTheme()
   const [open, setOpen] = React.useState(false)
 
-  const handleDrawerOpen = () => {
-    setOpen(true)
-  }
-
-  const handleDrawerClose = () => {
-    setOpen(false)
+  const handleDrawerToggle = () => {
+    setOpen(!open)
   }
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-            })}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            FINANCEE
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <TopBar />
       <Drawer
         variant="permanent"
         className={clsx(classes.drawer, {
@@ -73,11 +42,7 @@ const AppDrawer = ({children}) => {
           }),
         }}
       >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </div>
+        <div className={classes.toolbar} />
         <Divider />
         <List>
           {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
@@ -89,12 +54,13 @@ const AppDrawer = ({children}) => {
         </List>
         <Divider />
         <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
+          <ListItem 
+            button
+            onClick={handleDrawerToggle}
+          >
+            <ListItemIcon>{open ? <MenuCloseIcon /> : <MenuOpenIcon />}</ListItemIcon>
+            <ListItemText primary={open ? "Свернуть" : "Развернуть"} />
+          </ListItem>
         </List>
       </Drawer>
       <main className={classes.content}>
