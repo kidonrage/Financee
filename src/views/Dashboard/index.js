bc087d4d4477afcd8840c1efcf69cb47ab630fd2
Vlad Eliseev <freshymanchan@gmail.com>
2020-04-28 20:13:18 +1000

    Implement user's name displaying

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PurposeProgress from '../../components/PurposeProgress'
 import { Grid, Typography, makeStyles } from '@material-ui/core'
 import styles from './styles'
 import FactPlanDifference from '../../components/FactPlanDifference'
 import MonthOverview from '../../components/MonthOverview'
+import { AuthContext } from '../../components/Auth'
 
 const useStyles = makeStyles(styles)
 
 const Dashborad = () => {
   const classes = useStyles()
+  const {currentUser} = useContext(AuthContext)
 
   return (
     <>
-      <Typography variant="h2" component="h2" className={classes.header}>Добро пожаловать, Влад!</Typography>
+      <Typography variant="h2" component="h2" className={classes.header}>Добро пожаловать, {currentUser.displayName}!</Typography>
       <Grid container spacing={3} alignItems="stretch">
         <Grid className={classes.bottomSpacingGrid} item xs={12} md={6} direction="column" alignItems="stretch" container>
           <Grid item>
